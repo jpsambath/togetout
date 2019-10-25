617fa87563b0d8fecd1420b5c5c2ea13e69da48c
Antoine <antoine.mehaul29@orange.fr>
2019-10-25 16:59:18 +0200

    Antoine

--- tests/AntoineTestControllerTest.php
+++ tests/AntoineTestControllerTest.php
@@ -6,27 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AntoineTestControllerTest extends WebTestCase
 {
-    /*public function testCreerSortie()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/listeSorties');
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $sortie=json_decode($client->getResponse()->getContent(),true);
-        //var_dump($sortie);
-        //$this->assertEquals('Erreur lors de la validation !',$sortie['messageErreur']);
-        //$this->assertContains('Hello World', $crawler->filter('h1')->text());
-    }*/
-
     public function testCreerSortie()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/listeSorties');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $sortie=json_decode($client->getResponse()->getContent(),true);
+        //$sortie=json_decode($client->getResponse()->getContent(),true);
         //var_dump($sortie);
         //$this->assertEquals('Erreur lors de la validation !',$sortie['messageErreur']);
-        //$this->assertContains('Hello World', $crawler->filter('h1')->text());
     }
+
+    /*public function testCreerSortie()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/creerSortie');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        //$sortie=json_decode($client->getResponse()->getContent(),true);
+    }*/
+
+ /*   public function testSendMailRecuperationMDP()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/sendMailRecuperationMDP');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        //$sortie=json_decode($client->getResponse()->getContent(),true);
+        //var_dump($sortie);
+        //$this->assertEquals('Erreur lors de la validation !',$sortie['messageErreur']);
+    }*/
 }

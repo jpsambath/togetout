ba0e473d4942c98c18ed25bd0cf1d0c6dd9f9112
Antoine <antoine.mehaul29@orange.fr>
2019-10-25 13:57:22 +0200

    Antoine : Sortie

--- tests/AntoineTestControllerTest.php
+++ tests/AntoineTestControllerTest.php
@@ -6,6 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AntoineTestControllerTest extends WebTestCase
 {
+    /*public function testCreerSortie()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/listeSorties');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $sortie=json_decode($client->getResponse()->getContent(),true);
+        //var_dump($sortie);
+        //$this->assertEquals('Erreur lors de la validation !',$sortie['messageErreur']);
+        //$this->assertContains('Hello World', $crawler->filter('h1')->text());
+    }*/
+
     public function testCreerSortie()
     {
         $client = static::createClient();
